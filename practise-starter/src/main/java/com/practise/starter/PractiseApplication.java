f68ef18be857a0c4a3ff8c068546c5a7f3e04920
longyulan <754864626@qq.com>
2023-04-14 14:09:22 +0800

    hello

--- practise-starter/src/main/java/com/practise/starter/PractiseApplication.java
+++ practise-starter/src/main/java/com/practise/starter/PractiseApplication.java
@@ -2,15 +2,22 @@ package com.practise.starter;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.*;
 
 /**
  * @author longyulan
  * @date 2023/4/14
  */
 @SpringBootApplication
+@RestController
 public class PractiseApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(PractiseApplication.class);
+        SpringApplication.run(PractiseApplication.class, args);
+    }
+
+    @GetMapping("/hello")
+    public String hello(@RequestParam String name) {
+        return "hello " + name;
     }
 }
