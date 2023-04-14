afca7f27d0296497e5852e58aeb5a1539910c62e
longyulan <754864626@qq.com>
2023-04-14 11:53:51 +0800

    项目初始化

---
+++ practise-starter/src/main/java/com/practise/starter/PractiseApplication.java
@@ -0,0 +1,16 @@
+package com.practise.starter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author longyulan
+ * @date 2023/4/14
+ */
+@SpringBootApplication
+public class PractiseApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PractiseApplication.class);
+    }
+}
