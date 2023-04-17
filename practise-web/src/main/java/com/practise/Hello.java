67c677d1d5088e233add9b26d116683d7cbeeb86
longyulan <754864626@qq.com>
2023-04-17 15:42:06 +0800

    hello

---
+++ practise-web/src/main/java/com/practise/Hello.java
@@ -0,0 +1,11 @@
+package com.practise;
+
+/**
+ * @author longyulan
+ * @date 2023/4/17
+ */
+public class Hello {
+    public static void main(String[] args) {
+        System.out.println("hello......");
+    }
+}
