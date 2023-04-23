fc0955fd6c753c7fa7eaac2a2bd0880ddd880759
longyulan <754864626@qq.com>
2023-04-23 15:12:28 +0800

    hello

--- practise-bootstrap/src/main/java/com/practise/PractiseApplication.java
+++ practise-bootstrap/src/main/java/com/practise/PractiseApplication.java
@@ -1,23 +1,21 @@
-package com.practise.starter;
+package com.practise;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 /**
+ * 启动类
+ *
  * @author longyulan
  * @date 2023/4/14
  */
-@SpringBootApplication
-@RestController
+@EnableDiscoveryClient
+@SpringBootApplication(scanBasePackages = "com.practise")
 public class PractiseApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(PractiseApplication.class, args);
     }
 
-    @GetMapping("/hello")
-    public String hello(@RequestParam String name) {
-        return "hello " + name;
-    }
 }
