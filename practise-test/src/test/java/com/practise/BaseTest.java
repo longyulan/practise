895ccc29467dc823a2b03e223b90921fe1cfd1a8
longyulan <754864626@qq.com>
2023-04-24 10:36:14 +0800

    hello

---
+++ practise-test/src/test/java/com/practise/BaseTest.java
@@ -0,0 +1,11 @@
+package com.practise;
+
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest(classes = PractiseApplication.class)
+public class BaseTest {
+
+}
