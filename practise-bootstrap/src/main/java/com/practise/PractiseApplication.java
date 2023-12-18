5c8747023574f153a976a3049d3d1ea2c9668743
longyulan <754864626@qq.com>
2023-12-18 14:45:41 +0800

    update

--- practise-bootstrap/src/main/java/com/practise/PractiseApplication.java
+++ practise-bootstrap/src/main/java/com/practise/PractiseApplication.java
@@ -2,6 +2,8 @@ package com.practise;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 /**
@@ -12,10 +14,14 @@ import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
  */
 @EnableDiscoveryClient
 @SpringBootApplication(scanBasePackages = "com.practise")
-public class PractiseApplication {
+public class PractiseApplication extends SpringBootServletInitializer {
 
     public static void main(String[] args) {
         SpringApplication.run(PractiseApplication.class, args);
     }
 
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+        return builder.sources(PractiseApplication.class);
+    }
 }
